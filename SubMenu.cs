e76190e046fac252fc72533a9bdeb6676f0923ba
Dominik <minikra@o2.pl>
2012-02-28 19:53:07 +0100

    menu_poczatki2

---
+++ SubMenu.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gra
+{
+    class SubMenu
+    {
+        public String MenuName;
+        public bool Enabled;
+        public bool Selected;
+        public SubMenu Parent;
+        public List<SubMenu> SubMenus;
+        public event Action Actions;
+        public bool Ending;
+
+        public SubMenu()
+        {
+            SubMenus = new List<SubMenu>();
+            Enabled = true;
+            Selected = false;
+            Ending = false;
+        }
+
+        public void AddSub(SubMenu subMenu)
+        {
+            SubMenus.Add(subMenu);
+        }
+
+        public void AddAction(Action action)
+        {
+            Actions += action;
+        }
+
+        public void CallActions()
+        {
+            Actions();
+        }
+    }
+}
