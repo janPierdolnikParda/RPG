3fd955bc5618c27d57d2f148c922ffd337020d4d
RF <rafiq666@gmail.com>
2012-04-27 21:31:56 +0200

    poryciak

---
+++ ItemShield.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mogre;
+
+namespace Gra
+{
+	public class ItemShield : DescribedProfile
+	{
+		public bool InUse;
+		public float Blok;
+		public Vector3 HandleOffset;
+
+		public new ItemShield Clone()
+		{
+			return (ItemShield)MemberwiseClone();
+		}
+
+		public ItemShield(DescribedProfile prof)
+		{
+			MeshName = prof.MeshName;
+		}
+	}
+}
